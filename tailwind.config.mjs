9d8bff85f7067f9f9f80f2fd74e57d175365d2b4
Brun02K23 <bvirinni@gmail.com>
2024-03-14 00:24:17 -0300

    accesibilidad

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -9,7 +9,13 @@ export default {
 				sm: '0 1px 2px var(--tw-shadow-color)',
 				DEFAULT: '0 2px 4px var(--tw-shadow-color)',
 				lg: '0 8px 16px var(--tw-shadow-color)',
-			}
+			},
+			fontFamily: {
+				'twinkle-star': ['"Twinkle Star"', 'sans-serif'],
+				angkor: ['Angkor', 'sans-serif'],
+				oswald: ['Oswald', 'sans-serif'],
+				'indie-flower': ['Indie Flower', 'cursive'],
+			},
 		},
 	},
 	darkMode: 'class',
